d4ec9b7848fe2743b5efdd8e8e82fc04a99aa285
princemwill <princemwilliams@gmail.com>
2017-03-31 14:15:32 -0400

    Modified CodeEval script to split out input values

---
+++ FizzBuzz/FzBz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FzBz
+    {
+        //based of codeeval read from file code
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(@"C:\Users\princ\Documents\Notes\CodeEval_fizzBuzz.txt"))
+                while (!reader.EndOfStream)
+                {
+                    string input = reader.ReadLine();
+                    if (null == input)
+                        continue;
+
+                    string[] fbList = input.Split(' ');
+                    Console.WriteLine(fbList[2]);
+                   
+                }
+
+
+            Console.ReadKey();
+
+
+            // Works too keeping incase proves to be useful
+            //IEnumerable<string> input = File.ReadLines(@"C:\Users\princ\Documents\Notes\CodeEval_fizzBuzz.txt");
+
+
+            //foreach (var fbInstructionLine in input)
+            //{
+
+            //    string[] fbList = fbInstructionLine.Split(' ');           
+            //    Console.WriteLine(fbList[2]);
+
+            //      This portionn gives the same output as Conosle.WriteLine(input); does in above method
+            //    Console.WriteLine(fbInstructionLine);
+            //}
+            //Console.ReadKey();
+
+        }
+
+        public string FizzBuzzIt()
+        {
+
+        }
+    }
+}
---
+++ FizzBuzzTest/FzBzTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class FzBzTest
+    {
+        [TestMethod]
+        public void FizzTest()
+        {
+            FzBzTest fizz = new FizzBuzz.FzBz();
+            var expected = "F";
+            //act
+
+
+            //assert
+
+
+        }
+    }
+}
