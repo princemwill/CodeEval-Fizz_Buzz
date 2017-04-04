a62ad618ebfed6f309d0748c8cc326e05fe6b253
princemwill <princemwilliams@gmail.com>
2017-04-04 10:06:51 -0400

    Fixes so output prints out one line per set.

--- FizzBuzz/FzBz.cs
+++ FizzBuzz/FzBz.cs
@@ -8,11 +8,10 @@ using System.Threading.Tasks;
 namespace FizzBuzz
 {
     class FzBz
-    {
-        //based of codeeval read from file code
+    {        
         static void Main(string[] args)
         {
-            using (StreamReader reader = File.OpenText(@"C:\Users\princ\Documents\Notes\CodeEval_fizzBuzz.txt"))
+            using (StreamReader reader = File.OpenText(@"C: \Users\princ\Documents\Notes\CodeEval_fizzBuzz2.txt"))
                 while (!reader.EndOfStream)
                 {
                     string input = reader.ReadLine();
@@ -20,21 +19,18 @@ namespace FizzBuzz
                         continue;
 
                     string[] fbArguments = input.Split(' ');
-                    //Console.WriteLine(fbList[2]);
 
                     int X = int.Parse(fbArguments[0]);
                     int Y = int.Parse(fbArguments[1]);
                     int N = int.Parse(fbArguments[2]);
 
-                    // Console.WriteLine(X);
+                    var fizzBuzzOut = new StringBuilder();
 
                     for (int i = 1; i <= N; i++)
                     {
                         bool FizzIt = i % X == 0;
                         bool BuzzIt = i % Y == 0;
                         bool FizzBuzzIt = FizzIt && BuzzIt;
-
-                        var fizzBuzzOut = new StringBuilder();
 
                         if (FizzBuzzIt)
                         {
@@ -53,8 +49,8 @@ namespace FizzBuzz
                             fizzBuzzOut.Append(i.ToString());
                         }
 
-                        Console.WriteLine(fizzBuzzOut);
                     }
+                    Console.WriteLine(fizzBuzzOut);
                 }
 
 
